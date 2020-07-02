559a3b1cb4bd69c5f07ca742d9dcd66636e4e750
forellendev <forellendev@gmail.com>
2020-07-02 23:43:42 +0200

    Add command line input

--- todo/todo.js
+++ todo/todo.js
@@ -6,3 +6,14 @@ const chalk = require('chalk');
 console.log(validator.isEmail('Mike@example.com'));
 console.log(validator.isURL('https://google.com'));
 console.log(chalk.green.bgWhite.bold('Success'));
+
+// Path to node executable, file and params
+console.log(process.argv);
+
+const command = process.argv[2];
+
+if (command === 'add') {
+    console.log('Adding note...');
+} else if (command === 'remove') {
+    console.log('Removing note...');
+}
