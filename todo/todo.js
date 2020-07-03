911351fb0d9aa647a4560a494b5d3239d9fbe02d
forellendev <forellendev@gmail.com>
2020-07-03 23:32:42 +0200

    Add basic commands

--- todo/todo.js
+++ todo/todo.js
@@ -2,18 +2,51 @@ const add = require('./utilities.js');
 const notes = require('./notes.js');
 const { default: validator } = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
+
+yargs.version('2.0.0');
 
 console.log(validator.isEmail('Mike@example.com'));
 console.log(validator.isURL('https://google.com'));
 console.log(chalk.green.bgWhite.bold('Success'));
 
 // Path to node executable, file and params
-console.log(process.argv);
+// console.log(process.argv);
 
-const command = process.argv[2];
+// Add note
+yargs.command({
+    command: 'add',
+    describe: 'Add new note.',
+    handler: function() {
+        console.log('Note added.');
+    }
+});
 
-if (command === 'add') {
-    console.log('Adding note...');
-} else if (command === 'remove') {
-    console.log('Removing note...');
-}
+// Delete note
+yargs.command({
+    command: 'delete',
+    describe: 'Delete a note.',
+    handler: function() {
+        console.log('Note removed.');
+    }
+});
+
+// Read note
+yargs.command({
+    command: 'read',
+    describe: 'Read a note.',
+    handler: function() {
+        console.log('Read a note');
+    }
+});
+
+// List all notes
+yargs.command({
+    command: 'list',
+    describe: 'List all notes.',
+    handler: function() {
+        console.log('List all notes');
+    }
+});
+
+console.log(yargs.argv);
